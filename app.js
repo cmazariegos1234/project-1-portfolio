d84a3f7cd84c1605f95fcf0ca69cf7405d7a7ee9
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2020-07-15 17:24:00 -0400

    working on google sheet email form

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //==NAV BAR==
 
-let firstDiv = $(".navbar").append('<div class ="brand-title"> Brand Name</div>')
+let firstDiv = $(".navbar").append('<div class ="brand-title"> Claudia Mazariegos</div>')
 let firstAttr = $(".navbar").append('<a href ="#" class="toggle-button"><span class="bar"></span> <span class="bar"></span> <span class="bar"></span> </a>')
 let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a href="#">Home</a></li><li><a href="#">About</a></li><li><a href="#">Contact</a></li></ul></div>')
 
@@ -10,12 +10,11 @@ let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a href="
 const toggleButton = document.getElementsByClassName('toggle-Button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// })  //
 
 //==NAV BAR end ==
-
 
 
 //==================JSON FOR PORTFOLIO (PAGE 3)===============================
@@ -56,8 +55,63 @@ fetch(url)
             //$('body').append(createProjectElement(data[0]))
             data.forEach(project => {
                 const $projectDiv = createProjectElement(project)
-                $('body').append($projectDiv)
+                $('.body').append($projectDiv)
             })
         }
         
 //==================JSON FOR PORTFOLIO (PAGE 3) END===============================
+
+
+
+//=======Email Submission======
+
+let $firstEmailDiv = $(".newDiv").append('<div class ="wrapper"> <h2> Contact Us</h2></div>')
+
+let $secondEmailDiv = $(".wrapper").append('<div id ="error_message"></div>')
+
+const $form = $(".wrapper").append('<form id ="myform">')
+const $firstFormDiv = $("#myform").append('<div class="input_field"><input type="text" placeholder="name" id="name"></input></div>')
+const $secondFormDiv = $("#myform").append('<div class="input_field"><input type="text" placeholder="subject" id="subject"></input></div>')
+const $thirdFormDiv = $("#myform").append('<div class="input_field"><input type="text" placeholder="phone" id="phone"></input></div>')
+const $fourthFormDiv = $("#myform").append('<div class="input_field"><input type="text" placeholder="email" id="email"></input></div>')
+const $formTextArea = $("#myform").append('<div class="input_field"><textarea id="message" placeholder="Message"></textarea></div>')
+const $formSubmitButton = $("#myform").append('<div class="btn"><input type="submit"</div>')
+
+
+function validation() {
+    let name = document.getElementById("name").value;
+    let subject = document.getElementById("subject").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let error_message = document.getElementById("error_message");
+    var text;
+
+    error_message.style.padding = "10px";
+
+    if(name.length < 5) {
+        text = "Please Enter Your Name"
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(isNaN(phone) || phone.length !=10) {
+        text = "Please Enter Phone Number";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(email.indexOf("@") == -1 || email.length  < 6) {
+        text = "Please Enter Phone Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+
+    if(message.length)
+
+    return false;
+
+}
+
+
+//=======Email Submission======

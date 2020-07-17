3785c4a09fb24eec6abc477bd77aa58bf0816a89
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2020-07-17 16:12:46 -0400

    fixed Skills Json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 let firstDiv = $(".navbar").append('<div class ="brand-title"> Claudia Mazariegos</div>')
 let firstAttr = $(".navbar").append('<a href ="#" class="toggle-button"><span class="bar"></span> <span class="bar"></span> <span class="bar"></span> </a>')
-let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a href="#">About Me</a></li><li><a href="#">Portfolio</a></li><li><a href="#">Contact</a></li></ul></div>')
+let secondDiv = $(".navbar").append('<div class="navbar-links"><ul><li><a href="#"><a class="aaa" href="#startabout">About Me</a></a></li><li><a href="#"><a class="aaa" href="#skills">Skills</a></a></li><li><a href="#"><a class="aaa" href="#portfolio">Portfolio</a></li><li><a href="#"><a class="aaa" href="#contact">Contact</a></a></li></ul></div>')
 
 //getting class from the dom 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
@@ -18,45 +18,45 @@ toggleButton.addEventListener('click', () => {
 
 //==================JSON FOR SKILLS (PAGE 3)===============================
 
-// console.log($)
+console.log($)
 
-// const url1 = 'https://spreadsheets.google.com/feeds/list/1V0A37HYtImxAfAye9-Ci0LPM86gSPVrSDRrjmIlv83A/od6/public/values?alt=json'
+const url1 = 'https://spreadsheets.google.com/feeds/list/1V0A37HYtImxAfAye9-Ci0LPM86gSPVrSDRrjmIlv83A/od6/public/values?alt=json'
 
-// fetch(url1)
-//       .then(response => response.json())
-//       .then(data => {
-//           //console.log('data:', data)
-//           console.log(data.feed.entry)
-//           const project2 = data.feed.entry.map(entry => {
-//             return {
-//                  title1: entry.gsx$title.$t,
-//                 image1: entry.gsx$image.$t,
-//                 description1: entry.gsx$description.$t,
-//                 url1: entry.gsx$url.$t
-//              }
-//           })
-//           // console.log(project2);
-//           app2(project2);
-//         })
+fetch(url1)
+      .then(response => response.json())
+      .then(data => {
+          //console.log('data:', data)
+          console.log(data.feed.entry)
+          const project2 = data.feed.entry.map(entry => {
+            return {
+                 title1: entry.gsx$title1.$t,
+                image1: entry.gsx$image1.$t,
+                description1: entry.gsx$description1.$t,
+                url1: entry.gsx$url1.$t
+             }
+          })
+          // console.log(project2);
+          app2(project2);
+        })
 
-//         const app2 = (data) => {
-//             console.log('app is running')
-//             console.log(data)
+        const app2 = (data) => {
+            console.log('app is running')
+            console.log(data)
 
-//             const createProjectElement2 = (project2) => {
-//                 const $div2 = $('<div class="skills-text">')
-//                 $div2.append($('<h2>').text(project2.title1))
-//                 $div2.append($('<p>').text(project2.description1))
-//                 $div2.append($('<img>').attr('src', project2.image1))
-//                 $div2.append($('<a>').attr('href', project2.url1))
-//                 return $div2
-//             }
-//             //$('body').append(createProjectElement(data[0]))
-//             data.forEach(project2 => {
-//                 const $projectDiv2 = createProjectElement2(project2)
-//                 $('.jsonskills').append($projectDiv2)
-//             })
-//         }
+            const createProjectElement2 = (project2) => {
+                const $div2 = $('<div class="skills-text">')
+                $div2.append($('<h2>').text(project2.title1))
+                $div2.append($('<p>').text(project2.description1))
+                $div2.append($('<img>').attr('src', project2.image1))
+                $div2.append($('<a>').attr('href', project2.url1))
+                return $div2
+            }
+            //$('body').append(createProjectElement(data[0]))
+            data.forEach(project2 => {
+                const $projectDiv2 = createProjectElement2(project2)
+                $('.jsonskills').append($projectDiv2)
+            })
+        }
         
 //==================JSON FOR SKILLS (PAGE 3) END===============================
 
@@ -114,7 +114,7 @@ fetch(url)
 
 //=======GMAIL Email Submission======
 
-let $firstEmailDiv = $(".newDiv").append('<div class ="wrapper"> <h2> Get In Touch</h2></div>')
+let $firstEmailDiv = $(".newDiv").append('<div class ="wrapper"> <h2> <a name="contact" class="target1">Get In Touch</a></h2></div>')
 
 let $secondEmailDiv = $(".wrapper").append('<div id ="error_message"></div>')
 
